dce93b11b1df6dacb1ac434c767c64a5265da5a7
Grigiriy <grigiriy.malyshev@gmail.com>
2020-10-14 12:17:06 +0300

    status component (almost done)

--- src/components/elements/status/Status.jsx
+++ src/components/elements/status/Status.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './status.scss';
 
+import Angle from '../../../icons/angle.jsx';
+
 const Label = (props) => {
 return(
     <div className="status-wrapper">
-        <p>{props.status.status}</p>
-            <div>{props.status.option[0]}:<span>{props.status.option[1]}</span></div>
-            <div>Время до завершения:<span>{props.status.time_left}</span></div>
+        <p><span>{props.status.status}</span> <Angle /></p>
+        <div>{props.status.option[0]}:<span>{props.status.option[1]}</span></div>
+        <div>Время до завершения:<span>{props.status.time_left}</span></div>
     </div>
 )
 }
